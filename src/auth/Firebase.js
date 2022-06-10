9238c6018876c0311db52f259426e8e3d7dea976
Antonio Soares <asskct@gmail.com>
2022-06-10 09:51:17 -0300

    Update Firebase.js

--- src/auth/Firebase.js
+++ src/auth/Firebase.js
@@ -1,11 +1,11 @@
 define(() => {
   const Firebase = firebase.initializeApp({
-    apiKey: "AIzaSyBawxJUYZz1Nwx1M7yQY3Jy1TGD_tNg9Oo",
-    authDomain: "commonjs.firebaseapp.com",
-    projectId: "commonjs",
-    storageBucket: "commonjs.appspot.com",
-    messagingSenderId: "65803974707",
-    appId: "1:65803974707:web:47f6d0e142cf7b7ce2cb46"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   })
 
   const Firestore = firebase.firestore()
